e65462f37464eba07ab7dfe795531d1150888d11
andersonmlima <andersonmlima2@gmail.com>
2025-04-19 12:35:12 -0300

    primeiro commit do projeto

---
+++ src/main/java/com/anderson/triplyzer/TriplyzerApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.triplyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriplyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/triplyzer/TriplyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.triplyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
